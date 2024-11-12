a213f4968ac1ff3cb39ea1788685e2f2d240d84c
lizzie-liu <elizabethfj.liu@mail.utoronto.ca>
2024-11-12 12:27:05 -0500

    implemented classes for entities (Article, Edge)

---
+++ src/main/java/entity/Article.java
@@ -0,0 +1,49 @@
+package entity;
+
+/**
+ * This class represents the information about a research paper,
+ * and represents an Article node in our directed graph.
+ * It details the link to the article as well as the article's
+ * doi, title, authors, publication date, and references.
+ */
+public class Article {
+    private final String link;
+    private final String doi;
+    private final String title;
+    private final String[] authors;
+    private final String[] publication;
+    private final String[] references;
+
+    public Article(String doi, String title, String[] authors, String[] publication, String[] references) {
+        this.link = "https://doi.org/" + doi;
+        this.doi = doi;
+        this.title = title;
+        this.authors = authors;
+        this.publication = publication;
+        this.references = references;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDoi() {
+        return doi;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public String[] getPublication() {
+        return publication;
+    }
+
+    public String[] getReferences() {
+        return references;
+    }
+}
