a6c84a3c339f865a0d86b897b1e76535028dea20
trashb <ashleybi2005@gmail.com>
2024-11-12 15:50:58 -0500

    Added get() function for DAO

--- src/main/java/use_case/request_data/ApiDataAccessInterface.java
+++ src/main/java/use_case/request_data/ApiDataAccessInterface.java
@@ -1,8 +1,26 @@
 package use_case.request_data;
 
+import entity.Article;
+
 /**
  * DAO for the Api Data Request Use Case.
  */
 public interface ApiDataAccessInterface {
-    // get() method for getting articles (via API call)
+    /**
+     * Returns the set of articles matching the current query.
+     * @param sortType how the results should be sorted
+     * @param query the username to look up
+     * @return the set of articles.
+     */
+    Set<Article> get(String sortType, String query);
+
+    /**
+     * Returns the set of articles matching the current query in ascending order by relevance.
+     * @param query the username to look up
+     * @return the set of articles.
+     */
+    default Set<Article> getByRelevance(String query) {
+        return get("relevance", query);
+    }
+
 }
