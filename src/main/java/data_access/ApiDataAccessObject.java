5a310795a3fca1927546195239a040337a906a03
trashb <ashleybi2005@gmail.com>
2024-11-12 16:34:31 -0500

    re-implemented get() function
    input is now sortType and query (instead of DOI);
    output is now a set (hashset) of articles instead of just one article w/ corresponding doi

--- src/main/java/data_access/ApiDataAccessObject.java
+++ src/main/java/data_access/ApiDataAccessObject.java
@@ -1,15 +1,13 @@
 package data_access;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.StringJoiner;
+import java.util.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import entity.User;
+import entity.Article;
 import entity.UserFactory;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -25,7 +23,7 @@ import use_case.request_data.ApiDataAccessInterface;
  * The DAO for user data.
  */
 public class ApiDataAccessObject implements ApiDataAccessInterface {
-//        LoginUserDataAccessInterface,
+    //        LoginUserDataAccessInterface,
 //        ChangePasswordUserDataAccessInterface,
 //        LogoutUserDataAccessInterface {
     private static final String SUCCESS_CODE = "ok";
@@ -33,13 +31,19 @@ public class ApiDataAccessObject implements ApiDataAccessInterface {
     private static final String CONTENT_TYPE_JSON = "application/json";
     private static final String STATUS_CODE_LABEL = "status";
     private static final String MESSAGE = "message";
+    private static final String ITEMS = "items";
+
+    private static final String DOI = "DOI";
     private static final String TITLE = "title";
     private static final String AUTHOR = "author";
     private static final String GIVEN = "given";
     private static final String FAMILY = "family";
     private static final String DATE = "published";
     private static final String DATE_PARTS = "date-parts";
-    private final UserFactory articleFactory;
+    private static final String REFERENCE = "reference";
+
+    private static final int NUM_OUTPUTS = 10;
+    // private final UserFactory articleFactory;
     // TODO: -> Article Factory
 
     public ApiDataAccessObject(UserFactory userFactory) {
@@ -47,49 +51,69 @@ public class ApiDataAccessObject implements ApiDataAccessInterface {
         // No need to do anything to reinitialize a user list! The data is the cloud that may be miles away.
     }
 
-//    @Overrides
-    public User get(String doi) {
-        // TODO: make Article (node) class
+    @Override
+    public Set<Article> get(String sortType, String query) {
+        Set<Article> articles = new HashSet<>();
+
+        // format query for insertion into API request
+        final String urlQuery = query.replaceAll(" +", "+");
 
         // Make an API call to get the article object.
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
         final Request request = new Request.Builder()
-                .url("https://api.crossref.org/works/" + doi)
-                .addHeader("Content-Type", CONTENT_TYPE_JSON)
+                .url(String.format("https://api.crossref.org/works?query=%s"
+                                + "&select=DOI,title,published,author,reference"
+                                + "&sort=%s&order=desc", urlQuery, sortType))
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
                 .build();
+
         try {
             final Response response = client.newCall(request).execute();
-
             final JSONObject responseBody = new JSONObject(response.body().string());
 
             if (responseBody.getString(STATUS_CODE_LABEL).equals(SUCCESS_CODE)) {
-                final JSONObject resultJSONObject = responseBody.getJSONObject(MESSAGE);
-                final String title = resultJSONObject.getJSONArray(TITLE).getString(0);
-                // get authors
-                final JSONArray authorsJSONArr = resultJSONObject.getJSONArray(AUTHOR);
-                final String[] authors = new String[authorsJSONArr.length()];
-                for (int i = 0; i < authorsJSONArr.length(); i++) {
-                    final JSONObject author = authorsJSONArr.getJSONObject(i);
-                    authors[i] = author.getString(GIVEN) + " " + author.getString(FAMILY);
+                final JSONArray resultsJSONArray = responseBody.getJSONObject(MESSAGE).getJSONArray(ITEMS);
+                for (int i = 0; i < NUM_OUTPUTS; i++) {
+                    final JSONObject articleJSONObject = resultsJSONArray.getJSONObject(i);
+
+                    final String doi = articleJSONObject.getString(DOI);
+                    final String title = articleJSONObject.getJSONArray(TITLE).getString(0);
+                    final JSONArray authorsJSONArr = articleJSONObject.getJSONArray(AUTHOR);
+                    final String[] authors = new String[authorsJSONArr.length()];
+                    final StringJoiner date = new StringJoiner("/");
+                    final Set<String> references = new HashSet<>();
+
+                    // get authors
+                    for (int j = 0; j < authorsJSONArr.length(); j++) {
+                        final JSONObject author = authorsJSONArr.getJSONObject(j);
+                        authors[j] = author.getString(GIVEN) + " " + author.getString(FAMILY);
+                    }
+                    // get date
+                    final JSONArray dateJSONArr = articleJSONObject.getJSONObject(DATE).getJSONArray(DATE_PARTS)
+                            .getJSONArray(0);
+                    for (int k = 0; k < dateJSONArr.length(); k++) {
+                        date.add(Integer.toString(dateJSONArr.getInt(i)));
+                    }
+                    // get references (as set of DOIs)
+                    final JSONArray refsJSONArr = articleJSONObject.getJSONArray(REFERENCE);
+                    for (int l = 0; l < refsJSONArr.length(); l++) {
+                        references.add(refsJSONArr.getJSONObject(l).getString(DOI));
+                    }
+
+                    // Just for squiggly removal:
+                    System.out.println(title);
+                    System.out.println(authors[0]);
+                    System.out.println(date.toString());
+                    // TODO: use articleFactory?
+                    // articleFactory.create(doi, title, authors, date.toString(), references);
+                    articles.add(new Article(doi, title, authors, date.toString(), references));
                 }
-                // get date
-                final JSONArray dateJSONArr = resultJSONObject.getJSONObject(DATE).getJSONArray(DATE_PARTS)
-                        .getJSONArray(0);
-                final StringJoiner date = new StringJoiner("/");
-                for (int i = 0; i < dateJSONArr.length(); i++) {
-                    date.add(Integer.toString(dateJSONArr.getInt(i)));
-                }
-                // Just for squiggly removal:
-                System.out.println(title);
-                System.out.println(authors[0]);
-                System.out.println(date.toString());
-                // TODO: turn to articleFactory (remove password later)
-                // params: title, authors, publication date, doi
-                // return articleFactory.create(doi, title);
                 return null;
             }
+            // if there's an error code, return the error message:
             else {
-                throw new RuntimeException(responseBody.getString(MESSAGE));
+                throw new RuntimeException(responseBody.getJSONArray(MESSAGE).getJSONObject(0)
+                        .getString(MESSAGE));
             }
         }
         catch (IOException | JSONException ex) {
