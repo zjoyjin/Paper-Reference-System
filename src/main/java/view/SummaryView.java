bdb228fed1b7304228689c97181c135b10305e5d
zjoyjin <joyee.jin1977@gmail.com>
2024-11-25 19:55:49 -0500

    create a view panel for summarizting the content (create pull request?)

--- src/main/java/view/SummaryView.java
+++ src/main/java/view/SummaryView.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashSet;
 
-// GUI class for summarization view
 public class SummaryView extends JFrame {
 
     private JTextArea summaryArea;
