4330ea4519858ae65ac04dc8cbd10a0bd1cfa780
trashb <ashleybi2005@gmail.com>
2024-11-26 01:31:02 -0500

    Added edge creation logic to interactor

--- src/main/java/use_case/results/ResultsInteractor.java
+++ src/main/java/use_case/results/ResultsInteractor.java
@@ -1,5 +1,6 @@
 package use_case.results;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import entity.Article;
@@ -20,11 +21,24 @@ public class ResultsInteractor implements ResultsInputBoundary {
     public void execute(ResultsInputData inputData) {
         final String query = inputData.getQuery();
         final Set<Article> articles = dataAccessInterface.getArticles(query);
-        final Set<Edge> edges = dataAccessInterface.getEdges(query);
+        final Set<Edge> edges = createEdges(articles);
 
         final ResultsOutputData outputData = new ResultsOutputData(articles, edges, false);
 
         resultsPresenter.prepareSuccessView(outputData);
+    }
+
+    private Set<Edge> createEdges(Set<Article> articles) {
+        Set<Edge> edges = new HashSet<>();
+        Article[] articlesList = articles.toArray(new Article[articles.size()]);
+        for (int a = 0; a < articles.size(); a++) {
+            for (int b = a + 1; b < articles.size(); b++) {
+                if (articlesList[a].hasReference(articlesList[b])) {
+                    edges.add(new Edge(articlesList[a], articlesList[b]));
+                }
+            }
+        }
+        return edges;
     }
 
     /* @Override
