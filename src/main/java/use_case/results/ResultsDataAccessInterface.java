91126d9cb96043f952ae9095747f89dd0bc661de
trashb <ashleybi2005@gmail.com>
2024-11-26 01:30:33 -0500

    rewrote ResultsDataAccessInterface

--- src/main/java/use_case/results/ResultsDataAccessInterface.java
+++ src/main/java/use_case/results/ResultsDataAccessInterface.java
@@ -9,9 +9,19 @@ import entity.Edge;
  * DAI for the Showing Results Use Case.
  */
 public interface ResultsDataAccessInterface {
+    /**
+     * Returns the set of articles matching the current query.
+     * @param sortType how the results should be sorted
+     * @param query the username to look up
+     * @return the set of articles.
+     */
+    Set<Article> get(String sortType, String query);
 
-    Set<Article> getArticles(String query);
-
-    Set<Edge> getEdges(String query);
+    /**
+     * Returns the set of articles matching the current query; to be overloaded.
+     * @param query the username to look up
+     * @return the set of articles.
+     */
+    Set<Article> get(String query);
 
 }
