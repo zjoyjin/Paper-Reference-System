61bce9b81283de8d5f443c46d7079ebe88e9749c
zjoyjin <joyee.jin1977@gmail.com>
2024-12-01 13:42:14 -0500

    create SummmaryInteractor User case test

--- src/test/java/use_case/signup/SignupInteractorTest.java
+++ src/test/java/use_case/signup/SignupInteractorTest.java
@@ -14,7 +14,7 @@ class SignupInteractorTest {
 
     @Test
     void successTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "password");
+        SignupInputData inputData = new SignupInputData("Joyee", "password", "password");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
         // This creates a successPresenter that tests whether the test case is as we expect.
@@ -22,8 +22,8 @@ class SignupInteractorTest {
             @Override
             public void prepareSuccessView(SignupOutputData user) {
                 // 2 things to check: the output data is correct, and the user has been created in the DAO.
-                assertEquals("Paul", user.getUsername());
-                assertTrue(userRepository.existsByName("Paul"));
+                assertEquals("Joyee", user.getUsername());
+                assertTrue(userRepository.existsByName("Joyee"));
             }
 
             @Override
@@ -43,7 +43,7 @@ class SignupInteractorTest {
 
     @Test
     void failurePasswordMismatchTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
+        SignupInputData inputData = new SignupInputData("Joyee", "password", "wrong");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
         // This creates a presenter that tests whether the test case is as we expect.
@@ -71,12 +71,12 @@ class SignupInteractorTest {
 
     @Test
     void failureUserExistsTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
+        SignupInputData inputData = new SignupInputData("Joyee", "password", "wrong");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
         // Add Paul to the repo so that when we check later they already exist
         UserFactory factory = new CommonUserFactory();
-        User user = factory.create("Paul", "pwd");
+        User user = factory.create("Joyee", "pwd");
         userRepository.save(user);
 
         // This creates a presenter that tests whether the test case is as we expect.
